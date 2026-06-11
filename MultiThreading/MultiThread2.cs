69d186800a7e40bb4b6f207c0a859bba4a69af6b
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-06-11 15:53:38 +0530

    Add project files.

---
+++ MultiThreading/MultiThread2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadings
+{
+    class myclass
+    {
+        internal void fruitsdisplay()
+        {
+            string[] ar = new string[4] { "apple", "banana", "grapes", "orange" };
+            foreach (string s in ar)
+            {
+                Console.WriteLine("fruits :" + s);
+            }
+        }
+        internal void vegetablesdisplay()
+        {
+            string[] arr = new string[4] { "Onion", "carrot", "bottleguard", "potato" };
+            foreach (string V in arr)
+            {
+                Console.WriteLine("Vegetables :" + V);
+            }
+        }
+    }
+    internal class MultiThread2
+    {
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            ThreadStart tstart1 = new ThreadStart(obj.fruitsdisplay);
+            Thread tr1 = new Thread(tstart1);
+            tr1.Start();
+            
+            ThreadStart tstart2 = new ThreadStart(obj.vegetablesdisplay);
+            Thread tr2 = new Thread(tstart2);
+            tr2.Start();
+        }
+    }
+}
+
+
---
+++ MultiThreading/Program.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace MultiThreading
+//{
+
+//    class myclass
+//    {
+//        internal void method1()
+//        {
+//            for (int i = 0; i < 10; i++)
+//            {
+//                Console.WriteLine("method 1 is " + i);
+//            }
+//        }
+//        internal void method2()
+//        {
+//            for (int i = 0; i < 10; i++)
+//            {
+//                Console.WriteLine("method 2 is " + i);
+//            }
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass obj = new myclass();
+//            ThreadStart tstart1 = new ThreadStart(obj.method1);
+//            Thread thr1 = new Thread(tstart1);
+//            thr1.Start();
+//            ThreadStart tstart2 = new ThreadStart(obj.method2);
+//            Thread thr2 = new Thread(tstart2);
+//            thr2.Start();
+//        }
+//    }
+//}
